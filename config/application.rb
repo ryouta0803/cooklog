d94170ece7e5be2dd662b3b67ac637bf28486fed
ryouta0803 <67770359+ryouta0803@users.noreply.github.com>
2020-08-01 23:15:22 +0900

    rails generateの自動生成ファイルを設定

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,8 @@ module Cooklog
       g.assets false
       g.test_framework false
     end
+
+    
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
