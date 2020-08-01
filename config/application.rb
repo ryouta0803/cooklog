83dd405b77ab0dcfd7af10d814069548e6aebab1
ryouta0803 <67770359+ryouta0803@users.noreply.github.com>
2020-08-01 22:28:02 +0900

    Revert "rails generateの自動生成ファイルを設定"
    This reverts commit 5634445420d1b3c256f12a769e344e377ea93213.

--- config/application.rb
+++ config/application.rb
@@ -15,8 +15,6 @@ module Cooklog
       g.assets false
       g.test_framework false
     end
-
-    
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
