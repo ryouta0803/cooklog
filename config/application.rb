18cf29033d273fe6f1312bc8344c77bdebdb4fa8
ryouta0803 <67770359+ryouta0803@users.noreply.github.com>
2020-08-02 07:49:14 +0900

    テストを作成したときに自動生成が不要なファイルを指定

--- config/application.rb
+++ config/application.rb
@@ -16,7 +16,11 @@ module Cooklog
       g.test_framework false
     end
 
-    
+    g.test_framework :rspec,
+      controller_specs: false,
+      view_specs: false,
+      helper_specs: false,
+      routing_specs: false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
