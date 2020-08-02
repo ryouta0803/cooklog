cb92b57e39fd428e7d0d93af36046e6f6c8d9e91
ryouta0803 <67770359+ryouta0803@users.noreply.github.com>
2020-08-02 13:30:10 +0900

    ルーティングを追記

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
   get 'users/new'
   get 'users/show'
   get 'users/index'
+  get    :login,     to: 'sessions#new'
+  post   :login,     to: 'sessions#create'
+  delete :logout,    to: 'sessions#destroy'
+
   root 'static_pages#home'
   get :about,        to: 'static_pages#about'
   get :use_of_terms, to: 'static_pages#terms'
