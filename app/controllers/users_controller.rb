f767c452a556c091626dd62968416fca8eeb4b26
ryouta0803 <67770359+ryouta0803@users.noreply.github.com>
2020-08-02 15:17:57 +0900

    新しくユーザー登録した人が、登録と同時にログインできるようにした

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,6 +13,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      log_in @user # 追記
       flash[:success] = "クックログへようこそ！"
       redirect_to @user
     else
