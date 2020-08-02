9cb8f4588eaa2fc2e5fa064ce5c3630bcf7a50eb
ryouta0803 <67770359+ryouta0803@users.noreply.github.com>
2020-08-02 11:27:14 +0900

    ルーティング作成

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,7 @@ Rails.application.routes.draw do
   root 'static_pages#home'
   get :about,        to: 'static_pages#about'
   get :use_of_terms, to: 'static_pages#terms'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.
+  get :signup,       to: 'users#new'
+  resources :users
 end
